bc56c6bf7106e3f0c5034a63cdc0c5c526d707ae
Alanzote <arbunese@gmail.com>
2022-08-18 19:27:36 -0300

    Simplified Cache

---
+++ VM/Program.cs
@@ -0,0 +1,20 @@
+﻿// Usings...
+using VM;
+using VM.Interfaces;
+using VM.Implementations.Memory;
+
+// Create IO to Console.Out.
+IO io = new IO(Console.Out);
+
+// Create Memory, a 4 position cache with a 128 position RAM.
+IMemory mem = new Cache(4, new RAM(128));
+
+// Create our CPU.
+CPU cpu = new CPU(io, mem);
+
+// Write the Program.
+mem.Write(10, 120);
+mem.Write(11, 125);
+
+// Run program at address 10.
+cpu.Run(10);
